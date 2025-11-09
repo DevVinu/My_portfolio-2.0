3a5f95e932fc63a128e1cc11f9ed0f0b105bbe85
DevVinu <devvinudanadissa10@gmail.com>
2025-11-09 19:43:00 +0530

    change some stuff

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -9,8 +9,20 @@ import './index.css';
 import { projects } from './data/projects';
 
 export default function App() {
+  // To change the background video, just change this YouTube video ID
+  const youtubeVideoId = 'Hgg7M3kSqyE';
+
   return (
     <>
+      <div className="video-background">
+        <iframe
+          src={`https://www.youtube.com/embed/${youtubeVideoId}?autoplay=1&mute=1&loop=1&playlist=${youtubeVideoId}&controls=0&showinfo=0&autohide=1&modestbranding=1&iv_load_policy=3&playsinline=1`}
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="background-video"
+        ></iframe>
+      </div>
       <Header />
       <main>
         <Hero />
--- my-react-portfolio/src/components/Hero.jsx
+++ my-react-portfolio/src/components/Hero.jsx
@@ -8,8 +8,8 @@ export default function Hero() {
 				<div className="hero-content">
 					<img src={myImage} alt="Vinudana dissanayaka" className="hero-avatar" />
 					<div className="hero-text">
-						<h1>Vinudana dissanayaka</h1>
-						<p className="lead">This is my portfolio</p>
+						<h1>Vinudana Dissanayaka</h1>
+						<p className="lead">Hi!! this is my portforlio, Explor as you like so many content</p>
 						<a className="btn" href="#projects">See projects</a>
 					</div>
 				</div>
